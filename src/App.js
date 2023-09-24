0f897d5cd7f3079b9fa8e11a9880d25dc046bd52
shahabe9558 <shahabe9559@gmail.com>
2023-09-24 13:34:50 +0530

    set up the tailwind and react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App border border-red-950">
+     App
+    </div>
+  );
+}
+
+export default App;
